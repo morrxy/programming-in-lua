7de0322b9d88a69b4c5c589aabf3ad2c6687cd84
morrxy <morrxy@gmail.com>
2014-02-11 23:40:50 +0800

    m

--- chap-06/exercise-6-3.lua
+++ chap-06/exercise-6-3.lua
@@ -1,11 +1,13 @@
 --[[
 
-Exercise 6.3: Sometimes, a language with proper-tail calls is called properly tail
-recursive, with the argument that this property is relevant only when we have
-recursive calls. (Without recursive calls, the maximum call depth of a program
+Exercise 6.3: Sometimes, a language with proper-tail calls is
+called properly tail recursive, with the argument that this
+property is relevant only when we have recursive calls.
+(Without recursive calls, the maximum call depth of a program
 would be statically fixed.)
-Show that this argument does not hold in a dynamic language like Lua: write
-a program that performs an unbounded call chain without recursion. (Hint: see
-Section 8.1.)
+
+Show that this argument does not hold in a dynamic language like
+Lua: write a program that performs an unbounded call chain
+without recursion. (Hint: see Section 8.1.)
 
 ]]
