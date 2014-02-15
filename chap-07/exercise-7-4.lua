6cfa98b81371af239b95e3323007422d7bfa9a41
morrxy <morrxy@gmail.com>
2014-02-15 18:05:13 +0800

    m

--- chap-07/exercise-7-4.lua
+++ chap-07/exercise-7-4.lua
@@ -5,24 +5,39 @@ given string. (You will need the string.sub function.)
 
 ]]
 
-local iterator
+local function iterator( str, start )
+  start = start + 1
+
+  if (start <= #str) then
+    return start, string.sub(str, start, #str)
+  end
+end
 
 function substrings( str )
   return iterator, str, 0
 end
 
-function iterator( str, start )
-  print(type(start), start)
-  print(type(str), str)
-  start = start + 1
+local function iterator1(state)
+  state.loop_count = state.loop_count + 1
+  local current_loop_str = string.sub(state.loop_str, state.loop_count, #state.loop_str)
 
-  if (start <= #str) then
-    return string.sub(str, start, #str)
+  if (state.loop_count <= state.loop_times) then
+    return current_loop_str
   end
+end
+
+local function iterator2(state)
+  if (#state.loop_str > 0) then
+  end
+end
+
+function substrings1( str )
+  return iterator1, {loop_count = 0, loop_times = #str, loop_pos = 0, loop_str = str}
 end
 
 str1 = "go to school"
 
-for s in substrings(str1) do
-  print(s)
+for i in substrings1(str1) do
+  print(i)
 end
+
