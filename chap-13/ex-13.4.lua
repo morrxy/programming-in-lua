76c55088dbe167e9efab54bf609b6083144e13f9
morrxy <morrxy@gmail.com>
2014-03-07 13:05:18 +0800

    m

--- chap-13/ex-13.4.lua
+++ chap-13/ex-13.4.lua
@@ -7,3 +7,30 @@ tables is cheaper, as all read-only tables can share a single
 metatable. Rewrite function readOnly using this approach.
 
 ]]
+
+local index = {}
+
+local mt = {
+  __index = function (t, k)
+    return t[index][k]
+  end,
+
+  __newindex = function (t, k, v)
+    error("attempt to update a read-only table", 2)
+  end,
+}
+
+function readOnly(t)
+  local proxy = {}
+  proxy[index] = t
+  setmetatable(proxy, mt)
+  return proxy
+end
+
+days = readOnly{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}
+t1 = readOnly{k1 = "v1", k2 = "v2"}
+
+print(days[1])
+print(t1.k1)
+
+days[2] = "Noday"
