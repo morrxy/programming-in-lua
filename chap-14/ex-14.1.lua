e70b9ac1e9c7d469d926385b4a5dab495cf830a5
morrxy <morrxy@gmail.com>
2014-03-09 00:29:05 +0800

    a

--- chap-14/ex-14.1.lua
+++ chap-14/ex-14.1.lua
@@ -6,3 +6,20 @@ Rewrite it so that it accepts only single dots as name separators. (You may
 need some knowledge from Chapter 21 to do this exercise.)
 
 ]]
+
+function getfield(f)
+  local v = _G
+  for w in string.gmatch(f, "[^.]+") do
+    if (v[w]) then
+      print("found fiedl: " .. w)
+      v = v[w]
+    else
+      print("not exist that field: " .. w)
+      break
+    end
+  end
+  return v
+end
+
+print(getfield("io.read"))
+print(getfield("string!!!gsub.math?sin.sin"))
---
+++ chap-14/test.lua
@@ -0,0 +1,8 @@
+local a
+
+do
+  a = 2
+  print(a)
+end
+
+print(a)
