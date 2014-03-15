b70f2e65acb2e87de23ada0228cdb088b45a1649
morrxy <morrxy@gmail.com>
2014-03-15 11:25:39 +0800

    m

--- chap-16/ex-16.1.lua
+++ chap-16/ex-16.1.lua
@@ -9,15 +9,21 @@ Stack = {}
 
 Stack.new = function()
   local o = {}
-  setmetatable(o, {__index = self})
+  setmetatable(o, {__index = Stack})
   return o
 end
 
-Stack.push = function(e)
-  print("push: " .. e)
+function Stack:push(e)
+  print("push: " .. e .. " to " .. tostring(self))
+end
+
+Stack.pop = function(self)
+  print("pop from " .. tostring(self))
 end
 
 local s = Stack.new()
 
-s.push("ele")
+s.pop(s)
+
+s:push("ele")
 
